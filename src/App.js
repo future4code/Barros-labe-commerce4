ff5bbe737a5aae1ac1560ecbe5a82f8cef62e86e
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-07-17 12:38:59 -0300

    Só renderizei a SeçãoProdutos no App para vermos como está

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Cabeçalho, LayoutApp, SeçõesLoja } from './style';
+import SeçãoProdutos from './Components/SeçãoProdutos/SeçãoProdutos'
 
 function App() {
   return (
     <LayoutApp>
       <Cabeçalho><h1>Camisetas - E-commerce</h1></Cabeçalho>
       <SeçõesLoja>
+        <SeçãoProdutos/>
       </SeçõesLoja>
     </LayoutApp>
 
